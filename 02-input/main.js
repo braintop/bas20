429f1238f25bc53e74f2ddfcfb86cee02858cd05
ewfewfwef <braintop7@gmail.com>
2025-06-19 14:55:48 +0300

    Add

--- 02-input/main.js
+++ 02-input/main.js
@@ -1,2 +1,6 @@
-let x1 = prompt("enter number 1");
-console.log("u selected :  " + x1)
+let x1 = +prompt("enter number 1");//input
+
+let x2 = +prompt("enter number 2");//input
+let sol = x1 + x2
+console.log("x1 + x2 =  " + sol)//output
+alert(sol)//output 
