e4750053c590de49f07e1c1a9254104975bfa086
ewfewfwef <braintop7@gmail.com>
2025-06-29 15:14:40 +0300

    add

---
+++ 10-callback/10-disco/main.js
@@ -0,0 +1,9 @@
+function randomColor() {
+    let arr = ["green", "blue", "yellow", "white", "black"]
+    let index = Math.floor(Math.random() * arr.length)
+    console.log(arr[index])
+    document.body.style.background = arr[index]
+
+}
+setInterval(randomColor, 1000);
+
