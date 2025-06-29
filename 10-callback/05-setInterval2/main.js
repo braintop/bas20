28e73fb926b87822da45f8d0efc754f9492d7654
ewfewfwef <braintop7@gmail.com>
2025-06-29 15:06:10 +0300

    add

---
+++ 10-callback/05-setInterval2/main.js
@@ -0,0 +1,13 @@
+//כל 1000 מיליסקונד
+setInterval(() => {
+    let number = Math.floor(Math.random() * 100)
+    console.log(number)
+}, 1000);// תפעל כל שניה 
+
+//אחרי 1000 מיליסקונד 
+setTimeout(() => {
+    let number = Math.floor(Math.random() * 100)
+    console.log(number)
+}, 1000);// תפעל אחרי שניה 
+
+
