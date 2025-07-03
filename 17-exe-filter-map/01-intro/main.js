40d7b62ca0dc1efec6de56b10dd95865d103dc6b
ewfewfwef <braintop7@gmail.com>
2025-07-03 14:57:46 +0300

    add

--- 17-exe-filter-map/01-intro/main.js
+++ 17-exe-filter-map/01-intro/main.js
@@ -18,3 +18,33 @@ let cars = [
         electric: false
     }
 ];
+//1. 
+for (let car of cars) {
+    console.log(car)
+}
+//2. 
+for (let car of cars) {
+    console.log(car.model)
+}
+//3. 
+// let e = []
+// for(let car in cars){
+//     if(car.electric == true){
+//         e.push(car)
+//     }
+// }
+let electrics = cars.filter((item) => item.electric == true)
+console.log(electrics)
+//4 
+let newCars = cars.map((item) => {
+    if (item.electric) {
+        item.icon = "⚡️"
+    }
+    else {
+        item.icon = "⛽"
+    }
+    return item;
+})
+
+console.log(newCars)
+
