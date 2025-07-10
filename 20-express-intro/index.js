6b1d26fa82a5d56e91bcc7a937b925879e6a1d0f
ewfewfwef <braintop7@gmail.com>
2025-07-10 14:07:12 +0300

    add

---
+++ 20-express-intro/index.js
@@ -0,0 +1,16 @@
+let express = require("express")
+let app = express()
+let counter = 0
+
+app.get("/", (req, res) => {
+    res.status(200).send("hello express")
+})
+app.get("/plus", function (req, res) {
+    counter++
+    res.status(200).send("" + counter)
+})
+app.get("/minus", function (req, res) {
+    counter--
+    res.status(200).send("" + counter)
+})
+app.listen(3000)
