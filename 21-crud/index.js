fe7d795d64968f2339c314505357bdb21c500534
ewfewfwef <braintop7@gmail.com>
2025-07-10 15:36:34 +0300

    add

--- 21-crud/index.js
+++ 21-crud/index.js
@@ -1,10 +1,32 @@
 let express = require("express")
-//crud - create, read , update , delete 
+//.       items.  get.   get/:id.   
+//crud - create, read ,get by id,  update , delete 
 //.       post.   get.    put.    delete 
 
+let items = [
+    {
+        "id": 1,
+        "title": "ball",
+        "completed": false
+    },
+    {
+        "id": 2,
+        "title": "tshirt",
+        "completed": true
+    },
+    {
+        "id": 3,
+        "title": "Mezuza",
+        "completed": false
+    }
+]
 
 let app = express()
 app.get("/", function (req, res) {
-    res.status(200).send("hello world")
+    res.status(200).json(items)
 })
+app.get("/:id", function (req, res) {
+    res.status(200).json(items[req.params.id])
+})
+
 app.listen(3000)
