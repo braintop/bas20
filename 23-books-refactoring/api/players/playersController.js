40a5a1b3f3ff15c2d3158306544703a317d44884
ewfewfwef <braintop7@gmail.com>
2025-07-27 11:35:56 +0300

    add

---
+++ 23-books-refactoring/api/players/playersController.js
@@ -0,0 +1,14 @@
+[
+    {
+        "id": 0,
+        "firstname": "Andre",
+        "lastname": "Iguodala",
+        "age": 37,
+        "Team": "Warriors"
+    }, {
+        "id": 1,
+        "firstname": "Carmelo",
+        "lastname": "Anthony",
+        "age": 37,
+        "Team": "Lakers"
+    }]
