c6ad499bfd814fe50ba3f80bd0b393c24fc8f7d4
ewfewfwef <braintop7@gmail.com>
2025-07-27 14:38:40 +0300

    Add

--- 23-books-refactoring/api/players/playersController.js
+++ 23-books-refactoring/api/players/playersController.js
@@ -1,4 +1,4 @@
-[
+let players = [
     {
         "id": 0,
         "firstname": "Andre",
@@ -15,3 +15,64 @@
 //add get , post , delete update function to controller 
 //add router file
 //add to index referancr to routing file and test the server 
+
+exports.get = function (req, res) {
+    res.status(200).json(players)
+}
+
+exports.create = function (req, res) {
+    let player = {
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        teams: req.body.teams,
+        age: req.body.age
+    }
+    player.id = players.length == 0 ? 0 : players[players.length - 1].id + 1
+    players.push(player)
+    res.status(200).json(players)
+}
+
+exports.getById = function (req, res) {
+    let item = players.find((item) => item.id == req.params.id)
+    if (item == undefined) {
+        res.status(200).json({ message: "item not found" });
+        return;
+    }
+    console.log(item)
+    res.status(200).json(item)
+}
+
+
+exports.delete = function (req, res) {
+    let idToDelete = +req.params.id
+    let index = -1;// "1" === 1 false .    but "1" == 1 true 
+    for (let i = 0; i < players.length; i++) {
+        if (idToDelete === players[i].id) {
+            index = i;
+            break;
+        }
+    }
+    if (index == -1) {
+        res.status(200).json({ message: "item not found" })
+        return;
+    }
+    players.splice(index, 1);
+    res.status(200).json(players)
+
+}
+exports.update = function (req, res) {
+    let item = req.body;
+    let index = -1;// "1" === 1 false .    but "1" == 1 true 
+    for (let i = 0; i < players.length; i++) {
+        if (req.body.id == players[i].id) {
+            index = i;
+            break;
+        }
+    }
+    if (index == -1) {
+        res.status(200).json({ message: "item not found" })
+        return;//stop function
+    }
+    players[index] = item;
+    res.status(200).json(players)
+}
--- 23-books-refactoring/index.js
+++ 23-books-refactoring/index.js
@@ -4,6 +4,10 @@ app.use(express.json())
 
 var booksRouter = require("./api/books/bookRoute")
 app.use('/api/books', booksRouter);
+
+var playerRouter = require("./api/players/playerRoute")
+app.use('/api/players', playerRouter);
+
 app.listen(4000)
 
 
